3ab0300aed793d81436551db908e28f796c8bab6
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-07-30 00:49:49 +0530

    Fixed Windows version

--- include/fileDialog.h
+++ include/fileDialog.h
@@ -1,14 +1,13 @@
 #ifndef FILEDIALOG_H
 #define FILEDIALOG_H
-
 #include <iostream>
-#include <vector>
-#include <string>
+#include <filesystem>
 #include <GLFW/glfw3.h>
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+namespace fs = std::filesystem;
 
 bool showFileDialog(const char* label, std::string& selectedPath);
 
-#endif // FILEDIALOG_H
+#endif
