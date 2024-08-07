9affcc5fda9b7642e35d6d1a3a57bd7a00668ec5
rinsane <realrishabh9@gmail.com>
2024-08-07 13:36:16 +0530

    fixed for linux

--- imgui/imgui_init.h
+++ imgui/imgui_init.h
@@ -4,7 +4,7 @@
 #include "imgui_impl_glfw.h"
 #include "glm/glm.hpp"
 #include "imgui_impl_opengl3.h"
-#include "glfw/glfw3.h"
+#include "GLFW/glfw3.h"
 #include <iostream>
 #include <string>
 #include"appConfig.h"
